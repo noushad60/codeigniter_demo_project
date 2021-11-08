0e4c7bdbb490d51cda52aa4d32a9c530cabea4c0
root <root@localhost.localdomain>
2021-11-08 16:26:28 +0600

    confirmation

--- application/views/login/login_page.php
+++ application/views/login/login_page.php
@@ -4,16 +4,16 @@
         <form class="p-4" action="<?= base_url('login/login') ?>" method="post">
             <h6 class="mb-3" style="color: red"><?php echo $this->session->flashdata('msg'); ?></h6>
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Username</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" name="username">
+                <label for="user" class="form-label">Username</label>
+                <input type="text" class="form-control" id="user" name="username">
             </div>
             <div class="mb-3">
-                <label  class="form-label">Password</label>
+                <label  for="password" class="form-label">Password</label>
                 <input type="password" class="form-control" id="password" name="password">
             </div>
             <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" onclick="passShow()">
-                <label class="form-check-label" for="exampleCheck1">show Password</label>
+                <input id="checkBox" type="checkbox" class="form-check-input" onclick="passShow()">
+                <label class="form-check-label" for="checkBox">show Password</label>
             </div>
             <div class="mb-3 row justify-content-center">
                 <button type="submit" class="btn btn-primary">Login</button>
